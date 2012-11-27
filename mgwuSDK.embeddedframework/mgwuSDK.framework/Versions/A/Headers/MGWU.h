4eafa1e73a0a34fadca6f7fb238ddb5a9770d242
Ashu Desai <ashuddesai@gmail.com>
2012-11-26 21:33:51 -0500

    Bug Fixes

--- mgwuSDK.embeddedframework/mgwuSDK.framework/Versions/A/Headers/MGWU.h
+++ mgwuSDK.embeddedframework/mgwuSDK.framework/Versions/A/Headers/MGWU.h
@@ -7,7 +7,7 @@
 //
 //  Contains open source code and SDKs from Crashlytics, Inc. (SecureUDID, CrashlyticsSDK), Matej Bukovinski (MBProgressHUD), Stig Brautaset (SBJson), Ray Wenderlich (iAPHelper), Facebook (FacebookConnect iOS), Tapjoy (TapjoyConnect), Arash Payan (Appirater), Benjamin Borowski and Stephane Peter (GKAchievementNotification) thank you to all!
 //
-//  MGWU_BUILD_NUMBER 282
+//  MGWU_BUILD_NUMBER 283
 //
 
 #import <UIKit/UIKit.h>
