da4386ee7652c9ed74ee45b2c3635e5efd2de3e0
Ashu Desai <ashuddesai@gmail.com>
2013-01-17 00:18:51 -0800

    Added dark theme for facebook login, about and cross promo. Updated Instructions.

--- mgwuSDK.embeddedframework/mgwuSDK.framework/Versions/A/Headers/MGWU.h
+++ mgwuSDK.embeddedframework/mgwuSDK.framework/Versions/A/Headers/MGWU.h
@@ -7,7 +7,7 @@
 //
 //  Contains open source code and SDKs from Crashlytics, Inc. (SecureUDID, CrashlyticsSDK), Matej Bukovinski (MBProgressHUD), Stig Brautaset (SBJson), Ray Wenderlich (iAPHelper), Facebook (FacebookConnect iOS), Tapjoy (TapjoyConnect), Arash Payan (Appirater), Benjamin Borowski and Stephane Peter (GKAchievementNotification) thank you to all!
 //
-//  MGWU_BUILD_NUMBER 284
+//  MGWU_BUILD_NUMBER 295
 //
 
 #import <UIKit/UIKit.h>
@@ -20,13 +20,15 @@
 //General Setup:
 //
 + (void)loadMGWU:(NSString*)dev;
++ (void)dark;
 + (void)debug; //Depricated, has no use
 
 /////////////////////////////////////////////////////////////////////////////////
 //
 //Other Initializations
 //
-+ (void)useCrashlytics;
++ (void)useCrashlytics; //Depricated do not use
++ (void)useCrashlyticsWithApiKey:(NSString*)apiKey;
 + (void)setTapjoyAppId:(NSString*)tapappid andSecretKey:(NSString*)tapseckey;
 + (void)setAppiraterAppId:(NSString*)appappid andAppName:(NSString*)appappname;
 + (void)launchAppStorePage;
